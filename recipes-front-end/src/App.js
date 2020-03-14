519516519deb05d4a3036a482c33d090bf4fbc98
Evan James <thevan777@gmail.com>
2020-03-14 15:37:14 -0400

    displayed recipe on page

--- recipes-front-end/src/App.js
+++ recipes-front-end/src/App.js
@@ -1,19 +1,57 @@
 import React, {Component} from 'react';
 
+
+let baseUrl = ''
+
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = 'http://localhost:3003'
+} else {
+  baseUrl = 'heroku app'
+}
+
+console.log('current base URL:', baseUrl)
+
 class App extends Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            recipes: []
+        }
+        this.getRecipes = this.getRecipes.bind(this)
+    }
+    componentDidMount(){
+        this.getRecipes()
+    }
+    async getRecipes(){
+        try {
+            let response = await fetch(`${baseUrl}/recipes`)
+            let data = await response.json()
+            this.setState({recipes: data})
+        }catch(e){
+            console.error(e);
+        }
+    }
   render () {
+      console.log(this.state.recipes);
     return (
       <div>
         <h1>Recipes</h1>
+        <ul>
+            {
+                this.state.recipes.map(recipe => {
+                    return (
+                        <li key={recipe._id}>
+                            <h2>{recipe.name}</h2>
+                            <h3>Category: {recipe.category}</h3>
+                        </li>
+                    )
+                })
+            }
+        </ul>
       </div>
     )
   }
 }
-
-
-
-
-
 
 
 export default App;
--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const app = express()
 const PORT = 3003
 const mongoose = require('mongoose')
 const recipesController = require('./controllers/recipes.js')
+const cors = require('cors')
+
 
 // Middleware
 app.use(express.json())
+
+const whitelist = ['http://localhost:3000', 'heroku app']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
+
+
+
+
 
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
